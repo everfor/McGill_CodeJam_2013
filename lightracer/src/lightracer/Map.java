153a5817ab8f527b58b9aacbf563f51b3b3e3f0b
jungwankim <jungwan.kim@mail.mcgill.ca>
2013-11-01 17:15:02 -0400

    map class (without selector)
    map is implemented in 2d int array

--- lightracer/src/lightracer/Map.java
+++ lightracer/src/lightracer/Map.java
@@ -1,19 +1,72 @@
-/**
- * 
- */
+
 package lightracer;
 
-/**
- * @author jungwankim
- *
- */
+import java.io.*;
+import java.util.Scanner;
+
 public class Map {
 
-	/**
-	 * 
+	private final static int heightMap = 3;
+	private final static int widthMap = 3;
+	public final static int[][] boardMap = new int [widthMap][heightMap];
+	private File mapPath;
+	private static Scanner lineReader;
+	
+	/*
+	 * default map constructor
 	 */
+	
 	public Map() {
-		// TODO Auto-generated constructor stub
+		openMapfile();
+		readMapfile();
+		closeMapfile();
 	}
 
+	/*
+	public Map(File mapFile) {
+		selectMapfile(mapFile);
+		readMapfile();
+		closeMapfile();
+	}	
+	*/
+	
+	public void openMapfile() {
+		try {
+			mapPath = new File("").getAbsoluteFile();
+			lineReader = new Scanner(new File(mapPath + "/src/map/defaultMap.txt"));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readMapfile() {
+		try {
+			String[] temparyArray = new String[heightMap];
+			while (lineReader.hasNext()) {
+				for (int line = 0; line < heightMap; line++) {
+					temparyArray[line] = lineReader.nextLine();
+				}
+			}
+		
+			for (int yPixel = 0; yPixel < heightMap; yPixel++) {
+				for (int xPixel = 0; xPixel < widthMap; xPixel++) {
+					boardMap[xPixel][yPixel] = Integer.parseInt(temparyArray[yPixel].substring(xPixel, xPixel+1));
+				}
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeMapfile() {
+		lineReader.close();
+	}
+	
+	/*
+	public void selectMapfile() {
+		
+	}
+	*/
 }
